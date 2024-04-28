e92acc69182cd55a7e3241eafd7a133d9499b675
Alexander784 <alexandernyaga1999@gmail.com>
2024-04-28 22:26:42 +0300

    Install Tailwind and structure

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'blueColor':'#a2168ff',
+        'greyIsh':'#af1f4f8',
+        'cardShadow':'#f7f8f9',
+        'textColor':'#252a36',
+
+
+      }
+    },
+  },
+  plugins: [],
+}
+
