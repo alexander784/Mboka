6ad3bd08eb7d221abf855cb4aad750a2a4fdd3e9
Alexander784 <alexandernyaga1999@gmail.com>
2024-04-28 22:45:40 +0300

    Add Logo

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='navbar flex justify-between items-center p -{3rem}'>
+        <div className='logoDiv'>
+            <h1 className='logo text-[25px] text-blueColor'><strong>Crab</strong>Search</h1>
+            
+        </div>
+    </div>
   )
 }
 
