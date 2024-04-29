35495f48c378f19fc2b8c7a54842b4ddf1cbf271
Alexander784 <alexandernyaga1999@gmail.com>
2024-04-29 06:44:39 +0300

    Structure the Navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,10 +2,19 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div className='navbar flex justify-between items-center p -{3rem}'>
+    <div className='navbar flex justify-between items-center p-[3rem]'>
         <div className='logoDiv'>
             <h1 className='logo text-[25px] text-blueColor'><strong>Crab</strong>Search</h1>
-            
+            </div>
+            <div className='menu flex gap-8'>
+            <li className='menuList text-[#6f6ff6] hover:text blueColor'>Jobs</li>
+            <li className='menuList text-[#6f6ff6] hover:text blueColor'>Companies</li>
+            <li className='menuList text-[#6f6ff6] hover:text blueColor'>About</li>
+            <li className='menuList text-[#6f6ff6] hover:text blueColor'>Contact</li>
+            <li className='menuList text-[#6f6ff6] hover:text blueColor'>Blog</li>
+            <li className='menuList text-[#6f6ff6] hover:text blueColor'>Login</li>
+            <li className='menuList text-[#6f6ff6] hover:text blueColor'>Register</li>
+        
         </div>
     </div>
   )
