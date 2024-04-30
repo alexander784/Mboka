190e6844e45b039d65e4f63fe889c02efafc5699
Alexander784 <alexandernyaga1999@gmail.com>
2024-04-30 08:50:54 +0300

    Structure search input

--- src/components/searchdiv/Search.js
+++ src/components/searchdiv/Search.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { AiOutlineSearch } from "react-icons/ai";
 
 const Search = () => {
   return (
-    <div className='searchDiv grid gp-10 bg greish rounded-[18px] p-[3rem]'>
+    <div className='searchDiv grid gp-10 bg-greyish rounded-[18px] p-[3rem]'>
       <form action='firstDiv flex justify-between items-center rounded-[8x] gap-
       [10px] bg-white p-5 shadow-greish-700'>
         <div className='flex gap-2 items-center'>
+        <AiOutlineSearch className='text-[25px] icon '/>
+        <input type='text' className='bg-transparent text-blue-500
+        focus:outline-none w-[100%]' placeholder='Search job here...'/>
 
         </div>
 
       </form>
-
+  
     </div>
   )
 }
