d05d6e486b727768b4873afff4d6ed70e3c28362
Alexander784 <alexandernyaga1999@gmail.com>
2024-04-30 17:44:13 +0300

    Structure Sortby element

--- src/components/searchdiv/Search.js
+++ src/components/searchdiv/Search.js
@@ -9,7 +9,8 @@ import {BsHouseDoor  } from "react-icons/bs"
 const Search = () => {
   return (
     <div className='searchDiv grid gp-10 bg-greyish rounded-[18px] p-[3rem]'>
-      <form action='firstDiv flex justify-between items-center rounded-[8x] gap-
+      <form action=''>
+        <div className='firstDiv flex justify-between items-center rounded-[8x] gap-
       [10px] bg-white p-5 shadow-greish-700'>
         <div className='flex gap-2 items-center'>
         <AiOutlineSearch className='text-[25px] icon '/>
@@ -31,12 +32,24 @@ const Search = () => {
         focus:outline-none w-[100%]' placeholder='Search by location...'/>
           <AiOutlineCloseCircle className='text-[30px] text-[#a5a6a6]
               hover:text-textColor icon' />
+       
         </div>
-        
-
-      </form>
-  
+        <button className='bg-blueColor h-full p-5 px-10 rounded-[10px]
+        text-white cursor-pointer hover:bg-blue-300'>Search</button>
     </div>
+
+ 
+        
+      </form>
+      <div className='secDiv flex items-center gap-10 justify-center'>
+        <div className='singleSearch flex items-center gap-2'>
+          <label htmlFor='relevance' className='text-[#8080]' font-semibold>Sort By:</label>
+          
+        </div>
+      </div>
+      
+    </div>
+
   )
 }
 
