373fb0a58e7a6be807f50a386c6a5015bf9dec26
Alexander784 <alexandernyaga1999@gmail.com>
2024-05-01 11:44:37 +0300

    Responsive Value component

--- src/components/valuediv/Value.js
+++ src/components/valuediv/Value.js
@@ -1,79 +1,72 @@
-import React from 'react'
+import React from 'react';
 import simple from "../../assets/Simple.avif";
 
 const Value = () => {
   return (
-    <div className=' mb-[4rem] mt-[6rem]'>
-      <h1 className='text-textColor text-[25px] py-[2rem] pb-[3rem] font-bold w-[400px
-         block'>The value that holds true and to account</h1>
+    <div className='mb-16 mt-24'>
+      <h1 className='text-textColor text-3xl py-8 pb-12 font-bold md:w-3/4 mx-auto'>
+        The value that holds true and to account
+      </h1>
 
-         <div className='grid gap-[10rem] grid-cols-3 items-center'>
-          <div className='singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]'>
-            <div className='flex items-center gap-3'>
-              <div className='imgDiv p-[4px] rounded-[8rem] bg-[#dedef8] h-[40px]
-              w-[40px] flex items-center justify-center'>
-                <img src={simple} alt='' className='w-[70%]'/>
-              </div>
-              <span className='font-semibold text-textColor text-[18px]'>
-                Simplicity
-              </span>
+      <div className='grid gap-10 grid-cols-1 md:grid-cols-2 items-center mx-auto'>
+        <div className='singleGrid rounded-10 hover:bg-gray-100 p-6'>
+          <div className='flex items-center gap-3'>
+            <div className='imgDiv p-2 rounded-full bg-gray-200 h-10 w-10 flex items-center justify-center'>
+              <img src={simple} alt='' className='w-7/10' />
             </div>
-            <p className='text-[25px] text-textCOlor opacity-[7] py-[1rem]
-            font-semi'>
-              Things being made beautiful simple are at he heart of everything we do.
-
-            </p>
+            <span className='font-semibold text-textColor text-base'>
+              Simplicity
+            </span>
           </div>
-        
+          <p className='text-base text-textColor opacity-70 py-4 font-semibold'>
+            Things being made beautifully simple are at the heart of everything we do.
+          </p>
+        </div>
 
-          <div className='singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]'>
-            <div className='flex items-center gap-3'>
-              <div className='imgDiv p-[4px] rounded-[8rem] bg-[#dedef8] h-[40px]
-              w-[40px] flex items-center justify-center'>
-                <img src={simple} alt='' className='w-[70%]'/>
-              </div>
-              <span className='font-semibold text-textColor text-[18px]'>
-                Simplicity
-              </span>
+        <div className='singleGrid rounded-10 hover:bg-gray-100 p-6'>
+          <div className='flex items-center gap-3'>
+            <div className='imgDiv p-2 rounded-full bg-gray-200 h-10 w-10 flex items-center justify-center'>
+              <img src={simple} alt='' className='w-7/10' />
             </div>
-            <p className='text-[25px] text-textCOlor opacity-[7] py-[1rem]
-            font-semi'>
-             Easier and collaborative
-            </p>
+            <span className='font-semibold text-textColor text-base'>
+              Simplicity
+            </span>
           </div>
+          <p className='text-base text-textColor opacity-70 py-4 font-semibold'>
+            Easier and collaborative
+          </p>
+        </div>
 
-          <div className='singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]'>
-            <div className='flex items-center gap-3'>
-              <div className='imgDiv p-[4px] rounded-[8rem] bg-[#dedef8] h-[40px]
-              w-[40px] flex items-center justify-center'>
-                <img src={simple} alt='' className='w-[70%]'/>
-              </div>
-              <span className='font-semibold text-textColor text-[18px]'>
-                Simplicity
-              </span>
+        <div className='singleGrid rounded-10 hover:bg-gray-100 p-6'>
+          <div className='flex items-center gap-3'>
+            <div className='imgDiv p-2 rounded-full bg-gray-200 h-10 w-10 flex items-center justify-center'>
+              <img src={simple} alt='' className='w-7/10' />
             </div>
-            <p className='text-[25px] text-textCOlor opacity-[7] py-[1rem]
-            font-semi'>
-              Wrk from anywhere across the World.
-            </p>
+            <span className='font-semibold text-textColor text-base'>
+              Simplicity
+            </span>
           </div>
-         </div>
-         <div className='card mt-[2rem] flex justify-between bg-blueColor p-[5rem] rounded[10px]'>
-          <div>
-            <h1 className='text-bluecolor text-[30px] font-bold'>Ready to switch a career </h1>
+          <p className='text-base text-textColor opacity-70 py-4 font-semibold'>
+            Work from anywhere across the World.
+          </p>
+        </div>
+      </div>
 
-            <h2 className='text-textColor text-[25px] font-bold'>Lets get started</h2>
-
-          </div>
-          <button className='border-[2px] rounded-[10px] py-[4px] px-[50px] text-[10px]
-          font-semibold bg-white-500 hover:bg-cyan-600'>
-          Get Started </button>
-          </div>
-
-         
-
+      <div className='card mt-8 flex flex-col md:flex-row items-center justify-between  bg-cyan-500 p-20 rounded-10'>
+        <div>
+          <h1 className='text-white text-2xl font-bold'>
+            Ready to switch a career
+          </h1>
+          <h2 className='text-textColor text-xl font-bold'>
+            Let's get started
+          </h2>
+        </div>
+        <button className='border-2 rounded-10 py-2 px-10 text-sm font-semibold bg-white text-blue-500 hover:bg-blue-600 hover:text-white mt-8 md:mt-0'>
+          Get Started
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Value; 
+export default Value;
